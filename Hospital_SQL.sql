7425858e0fa8aa8593de5b3e3ab84a9021975385
qq920235807 <51893469+qq920235807@users.noreply.github.com>
2019-12-19 08:35:34 +0800

    Add files via upload

--- Hospital_SQL.sql
+++ Hospital_SQL.sql
@@ -26,17 +26,19 @@ create table Users
   UserAge int not null,--年龄
   UserPhone varchar(50) ,--电话
   UserEmile varchar(50) ,--邮箱
-  UserType int check(UserType=1 or UserType=2 or UserType=3),--角色(1=用户，2=医师，3=管理员)
+  UserType int check(UserType=1 or UserType=2 or UserType=3),--角色(1=，2=医师，3=管理员用户)
 )
+select UserType,count(UserType) as 数量 from Users  group by UserType
+select * from Users
 go
 insert into Users values ('张三','admin','admin','男',18,'17630036636','123123@qq.com',1)
+insert into Users values ('张三','1','2','男',18,'17630036636','123123@qq.com',1)
 insert into Users values ('张三','123','123','男',18,'17630036636','123123@qq.com',2)
 insert into Users values ('张三','456','456','男',18,'17630036636','123123@qq.com',3)
-select * from Users as u,TypeUser as r,MenuUser as m 
-where u.UserType=3 and r.menuID=1003
-
-select * from Users as u,TypeUser where u.UserID=TypeUser.TypeID and u.UserType=3
-select * from TypeUser,MenuUser where TypeUser.menuID=MenuUser.MenuTypeID and MenuUser.MenuTypeID =1002
+insert into Users values ('张三','789','789','男',19,'17630036636','123123@qq.com',3)
+delete from Users where UserID=5
+select * from Users
+select  UserAge, COUNT(UserAge) as number from Users group by UserAge
 --身份菜单表
 if exists(select * from sys.tables where name='TypeUser')
 drop table TypeUser
@@ -47,12 +49,19 @@ create table TypeUser
   TypeID int primary key identity(1,1),--用户ID
   menu varchar(20), --菜单
   menuID int, --菜单ID
+  MenuTypeID int, --二级菜单ID
   UserType int check(UserType=1 or UserType=2 or UserType=3),--角色(1=用户，2=医师，3=管理员)
 )
-insert into TypeUser values('一级菜单',1001,1)
-insert into TypeUser values('一级菜单',1002,2)
-insert into TypeUser values('一级菜单',1003,3)
-
+insert into TypeUser values('医院分布',1001,1009,1)
+insert into TypeUser values('医师管理',1001,1001,1)
+insert into TypeUser values('请假管理',1001,1002,1)
+insert into TypeUser values('病人管理',1001,1003,1)
+insert into TypeUser values('数据统计',1001,1008,1)
+insert into TypeUser values('病人处方',1002,1004,2)
+insert into TypeUser values('病人情况',1002,1005,2)
+insert into TypeUser values('我的订单',1003,1006,3)
+insert into TypeUser values('我的预约',1003,1007,3)
+select * from TypeUser where UserType=1
 --菜单表
 if exists(select * from sys.tables where name='MenuUser')
 drop table MenuUser
@@ -61,15 +70,28 @@ create table MenuUser
 (  
 MenuID int primary key identity(1,1),--用户ID
  Menu varchar(20), --菜单
- MenuTypeID int, --菜单ID
+ Url varchar(50) , --链接
+ MenuTypeID int, --二级菜单ID
 )
-insert into MenuUser values('aaa',1001)
-insert into MenuUser values('bbb',1001)
-insert into MenuUser values('ccc',1001)
-insert into MenuUser values('ddd',1002)
-insert into MenuUser values('qqq',1002)
-insert into MenuUser values('www',1002)
-insert into MenuUser values('eee',1003)
+insert into MenuUser values('全部医师','Javascript:void(0)',1001)
+insert into MenuUser values('已请假医师','Javascript:void(0)',1002)
+insert into MenuUser values('未审批','Javascript:void(0)',1002)
+insert into MenuUser values('全部病人','Javascript:void(0)',1003)
+insert into MenuUser values('未就诊','Javascript:void(0)',1003)
+insert into MenuUser values('挂号表','Javascript:void(0)',1003)
+insert into MenuUser values('统计图表','/Default/Echarts',1008)
+insert into MenuUser values('医院分布','/Default/Ditu',1009)
+
+
+
+insert into MenuUser values('已开药单','Javascript:void(0)',1004)
+insert into MenuUser values('全部病人','Javascript:void(0)',1005)
+insert into MenuUser values('未就诊','Javascript:void(0)',1005)
+insert into MenuUser values('已预约','Javascript:void(0)',1005)
+
+insert into MenuUser values('我的订单','Javascript:void(0)',1006)
+insert into MenuUser values('全部医师','Javascript:void(0)',1007)
+insert into MenuUser values('已预约','Javascript:void(0)',1007)
 
 --创建医师表
 if exists(select * from sys.tables where name='Doctor')
